f6e6887e0cc362bc90a55d5b848605901784a76c
hyoiizza <songhyojung0807@gmail.com>
2025-07-29 14:40:01 +0900

    deep ocsort aw, da fin

--- implement/deep_ocsort.py
+++ implement/deep_ocsort.py
@@ -125,8 +125,8 @@ def extract_feature_from_crop(bbox_crop_np, encoder_model, device='cpu'):
 
     return feature.squeeze(0).cpu().numpy()
 
-# ===================== Deep SORT 클래스 ====================
-class Deepsort:
+# ===================== Deep OCSORT 클래스 ====================
+class DeepOCsort:
     def __init__(self):
         # SORT 알고리즘 초기화
         # 추적중인 객체 리스트와 추적 ID 초기화
@@ -150,6 +150,7 @@ class Deepsort:
                         'bbox': [x1.item(), y1.item(), x2.item(), y2.item()],
                         'confidence': box.conf[0].item(),
                         'class': int(box.cls[0]),
+                        'feature' : 'not yet'
                     })
         return detections
 
@@ -165,39 +166,44 @@ class Deepsort:
         area2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
         return inter / float(area1 + area2 - inter + 1e-6) # 0으로 나누는 오류 방지
     
-    def mahalanobis_distance(self, trk_kf, detection_vec):
-        """
-        trk_kf: KalmanFilter 객체 (filterpy.kalman.KalmanFilter)
-        detection_vec: np.array([cx, cy, aspect_ratio, h]) shape=(4,)
-        """
-        kf = trk_kf.kf
+    def compute_ema_alpha(self, s_det, sigma=0.5, alpha_f=0.95):
+        if s_det <= sigma:
+            return 1.0
+        alpha_t = alpha_f + (1-alpha_f)*(1-(s_det-sigma)/(1-sigma))
+        return min(alpha_t, 1.0)
 
-        # 예측 관측값 (4x1)
-        y = np.dot(kf.H, kf.x)  # shape (4, 1)
-        y = y.flatten()                 # shape (4,)
+    def compute_aw(self, appearance, epsilon=0.5):
+        M,N = appearance.shape
+        z_track = np.zeros(M)
+        z_det = np.zeros(N)
 
-        # 예측 관측 공분산 행렬 S = HPH^T + R
-        S = np.dot(np.dot(kf.H, kf.P), kf.H.T) + kf.R  # shape (4, 4)
-        S_inv = np.linalg.inv(S)
-
-        # 거리 계산
-        diff = detection_vec - y
-        dist = np.dot(np.dot(diff.T, S_inv), diff)
-        return dist
-
-    def match(self, frame, detections, trackers, alpha=0.5,maha_threshold=9.4877 ,appearance_threshold=0):
+        for i in range(M):
+            row = appearance[i, :]
+            top1 = np.max(row)
+            top2 = np.max(row[row < top1] if np.any(row < top1) else top1)
+            z_track[i] = min(top1-top2,epsilon)
+        for j in range(N):
+            col = appearance[:,j]
+            top1 = np.max(col)
+            top2 = np.max(col[col<top1] if np.any(col<top1) else top1)
+            z_det[j] = min(top1 - top2, epsilon)
+        
+        w = (z_track[:, None] + z_det[None, :]) / 2.0
+        return w
+    
+    def match(self, frame, detections, trackers, appearance_weight=0.5,iou_threshold=0.3 ,appearance_threshold=0.5):
         if len(trackers) == 0: # 트래커가 없으면 매칭할 필요 없음
             return [], [], list(range(len(detections))) 
 
         trackers = list(range(len(self.tracked_objects)))
 
         cost_matrix = np.zeros((len(trackers), len(detections))) 
-        maha_dist = np.zeros((len(trackers), len(detections))) 
+        cost_iou = np.zeros((len(trackers), len(detections))) 
         appearance = np.zeros((len(trackers), len(detections)))
         appear_gate = np.zeros((len(trackers), len(detections)))
+        iou_gate = np.zeros((len(trackers), len(detections)))
         fin_gate = np.zeros((len(trackers), len(detections)))
-        maha_dist_gate = np.zeros((len(trackers), len(detections)))\
-        
+
         # tracker feature 저장
         tracker_features = []
         for i, trk_idx in enumerate(trackers):
@@ -207,8 +213,6 @@ class Deepsort:
             crop = frame[y1:y2, x1:x2] 
             feature = extract_feature_from_crop(crop, encoder, device='cpu')
             tracker_features.append(feature)
-            
-        
         # detection feature 저장
         detection_features = []
         for j, det in enumerate(detections):
@@ -217,86 +221,53 @@ class Deepsort:
             crop = frame[y1:y2, x1:x2]  
             feature = extract_feature_from_crop(crop, encoder, device='cpu')
             detection_features.append(feature)
-            detections[j]['feature'] = detection_features[j]
+            det['feature'] = feature
 
 
-        for i, trk_idx in enumerate(trackers):
-            trk = self.tracked_objects[trk_idx]
-            kf = trk['kf']  # KalmanBox 객체
+        for i, trk in enumerate(trackers):
+            trk = self.tracked_objects[trk]
             for j, det in enumerate(detections):
-                det_bbox = det['bbox']  # [x1, y1, x2, y2]
-                det_vec = bbox_to_kalman_state(det_bbox)[:4]  # [cx, cy, aspect, h]
-
-                # Mahalanobis distance 계산
-                maha = self.mahalanobis_distance(kf, det_vec)
-                maha_dist[i, j] = maha
-                if maha_dist[i,j] > maha_threshold: # 9.4877 = 95% 신뢰수준
-                    maha_dist_gate[i,j] = 1
-                else:
-                    maha_dist_gate[i,j] = 0
-
-                # cnn기반 외형정보 오차 계산
+                cost_iou[i, j] = 1 - self.iou(trk['bbox'], det['bbox']) 
                 appearance[i,j] = 1 - cosine_similarity(detection_features[j], tracker_features[i])
+                if cost_iou[i,j] >= iou_threshold:
+                    iou_gate[i,j] = 1
+                else:
+                    iou_gate[i,j] = 0
                 if appearance[i,j] > appearance_threshold:
                     appear_gate[i,j] = 1
                 else: 
-                    appear_gate[i,j] = 0
-                    
-                # 최종 cost 가중치 조합
-                fin_gate[i,j] = appear_gate[i,j] * maha_dist_gate[i,j]
+                    appear_gate[i,j] = 0 
+                fin_gate[i,j] = appear_gate[i,j]*iou_gate[i,j]
+                w = self.compute_aw(appearance)
                 if fin_gate[i,j] == 1:
-                    cost_matrix[i, j] = alpha * appearance[i,j] + (1 - alpha) * maha_dist[i,j]
+                    cost_matrix[i, j] = cost_iou[i,j] + (appearance_weight + w[i,j])*appearance[i,j]
                 else:
-                    cost_matrix[i, j] = 1e5  
+                    cost_matrix[i, j] = 1e5 
 
-        # time_since_update 리스트 생성
-        tracker_ages = [self.tracked_objects[t]['time_since_update'] for t in trackers]
+        row_ind, col_ind = linear_sum_assignment(cost_matrix) # 헝가리안 알고리즘으로 최적 매칭
 
         # 매칭된 트래커와 detection을 저장
         matches = []
         unmatched_trackers = list(range(len(trackers)))
-        unmatched_detections = list(range(len(detections)))
-        current_unmatched_dets = list(unmatched_detections)
+        unmatched_detections = list(range(len(detections))) 
 
-        max_age = max(tracker_ages) if tracker_ages else 0
-        for age in range(max_age + 1):
-            # 현재 age 그룹에 해당하는 트랙 인덱스만 추출
-            group_indices = [i for i, a in enumerate(tracker_ages) if a == age]
-            if not group_indices:
-                continue
-            # 해당 트랙 인덱스를 원래 trackers 인덱스로 매핑
-            group_tracker_indices = [trackers[i] for i in group_indices]
-
-            # 서브 cost 행렬 추출: group_tracker_indices x unmatched_detections
-            sub_cost = cost_matrix[np.ix_(group_indices, unmatched_detections)]
-            if sub_cost.size == 0:
-                continue
-
-            # time since update을 기준으로 우선 배정
-            row_ind, col_ind = linear_sum_assignment(sub_cost) # 헝가리안 알고리즘으로 최적 매칭
-
-            for r, c in zip(row_ind, col_ind):
-                trk_idx = group_tracker_indices[r]       # 전체 trackers 기준
-                det_idx = current_unmatched_dets[c]        # 전체 detection 기준
-
-                matches.append((trk_idx, det_idx))
-                if trk_idx in unmatched_trackers:
-                    unmatched_trackers.remove(trk_idx)
-                if det_idx in unmatched_detections:
-                    unmatched_detections.remove(det_idx)
+        # 매칭된 트래커와 detection을 IoU 임계값에 따라 필터링
+        for r, c in zip(row_ind, col_ind):
+            matches.append((r, c))
+            unmatched_trackers.remove(r)
+            unmatched_detections.remove(c)
 
         return matches, unmatched_trackers, unmatched_detections
 
 
-    def update(self, frame,detections,iou_threshold=0.3, ema_alpha = 0.9):
+    def update(self, frame ,detections ,frame_num, ema_alpha = 0.9):
         '''
             핵심 로직
             1. 기존 트래커들 predict()
             2. 매칭된 트래커 업데이트
-            3. 매칭 안 된 detection, trackers에 대해 두번째 매칭 (iou기반)
-            4. 두번째 매칭에 의해 매칭된 트래커 업데이트 
-            5. 두번째 매칭에 의해 새로 발견한 객체 업데이트(new track)
-            6. 트래커 삭제
+            3. re-id로 매칭된 tracker의 가상 궤적 보정 (OCR)
+            4. 두번째 매칭에 의해 새로 발견한 객체 업데이트(new track)
+            5. 트래커 삭제
         '''
         # 1. 기존 트래커 예측
         for trk in self.tracked_objects: # tracked_objects는 [{'id': id, 'kf': KalmanBox, 'bbox': [x1,y1,x2,y2]}, ...] 형태
@@ -310,77 +281,67 @@ class Deepsort:
                 print(f"bbox: {trk.get('bbox')}")
                 print(f"age: {trk.get('age')}")
                 print(f"time_since_update: {trk.get('time_since_update')}")
+                print(f"start_missing_frame_num: {trk.get('start_missing_frame_num')}")
                 print(f"[Kalman State] predict_s={trk['kf'].kf.x[2]}, ds={trk['kf'].kf.x[6]}")
-
+        
         matches, unmatched_trks, unmatched_dets = self.match(frame, detections, self.tracked_objects)
 
         #for m in matches:
             #print("match types:", type(m[0]), type(m[1]))
 
+        re_id = []
+        ema_alpha = ema_alpha
         # 2. 매칭된 트래커 업데이트
         for t, d in matches:
             track = self.tracked_objects[t]
             detection = detections[d]
-            if track['state'] == 'confirmed':
-                track['time_since_update'] = 0
-                track['bbox'] = track['kf'].update(detection['bbox'])
-                track['hits'] +=1
-                track['z_t_minus_2'] = track['z_t_minus_1']
-                track['z_t_minus_1'] = detection['bbox']
-                track['feature'] = ema_alpha*track['feature'] + (1-ema_alpha)*detection['feature']
-                track['feature'] /= np.linalg.norm(track['feature']) + 1e-6 # L2 정규화
-
-            if track['state'] == 'tentative':
-                track['hits'] +=1
+            s_det = detection['confidence']
+            ema_alpha = self.compute_ema_alpha(s_det, sigma=0.5, alpha_f=0.95) # DA 구현
+            if track['time_since_update'] >=1: # re-id의 첫 번째 조건
+                re_id.append((t,d))
+            else:
                 track['time_since_update'] = 0
                 track['bbox'] = track['kf'].update(detection['bbox'])
                 track['z_t_minus_2'] = track['z_t_minus_1']
                 track['z_t_minus_1'] = detection['bbox']
-                track['feature'] = ema_alpha*track['feature'] + (1-ema_alpha)*detection['feature']
-                track['feature'] /= np.linalg.norm(track['feature']) + 1e-6
+                feat_track = np.array(track['feature'], dtype=np.float32)
+                feat_det = np.array(detection['feature'], dtype=np.float32)
+                track['feature'] = ema_alpha*feat_track + (1-ema_alpha)*feat_det
+                feat_track = np.array(track['feature'], dtype=np.float32)
+                track['feature'] /= np.linalg.norm(feat_track) + 1e-6 # L2 정규화
 
-                if track['hits'] >=3:
-                    track['state'] = 'confirmed'
+         # 3. re-id로 매칭된 tracker의 가상 궤적 보정 (OCR)
+        for t,d in re_id:
+            track = self.tracked_objects[t]
+            detection = detections[d]
+            z_start = track['z_t_minus_1'] # re-id 되기 전의 마지막 tracking 값
+            z_end = detection['bbox'] # re-id 된 tracking 값
+            num_missing_frame = frame_num - track['start_missing_frame_num'] + 1 # 객체가 사라진 frame수 
+            feat_track = np.array(track['feature'], dtype=np.float32)
+            feat_det = np.array(detection['feature'], dtype=np.float32)
+            virtual_obs_list = []
+            for i in range(1,num_missing_frame+1):
+                alpha = i / (num_missing_frame+1)
+                z_virtual = (1 - alpha) * np.array(z_start) + alpha * np.array(z_end)
+                virtual_obs_list.append(z_virtual)
+            for z_virtual in virtual_obs_list:
+                track['kf'].predict()
+                track['bbox'] = track['kf'].update(z_virtual)
 
-        # 3. 매칭 안 된 detection, trackers에 대해 두번째 매칭 (iou기반)
-        cost_matrix_2 = np.zeros((len(unmatched_dets), len(unmatched_trks))) 
-        for i, d in enumerate(unmatched_dets):
-            for j, t in enumerate(unmatched_trks):
-                trk2 = self.tracked_objects[unmatched_trks[j]] # ========================
-                det2 = detections[unmatched_dets[i]] #=============================
-                cost_matrix_2[i, j] = 1 - self.iou(trk2['bbox'], det2['bbox']) 
+            # 마지막 진짜 detection으로 업데이트
+            track['kf'].predict()
+            track['bbox'] = track['kf'].update(z_end)
+            track['time_since_update'] = 0
+            track['z_t_minus_2'] = track['z_t_minus_1']
+            track['z_t_minus_1'] = z_end
+            track['start_missing_frame_num'] = 're-identified!'
+            track['feature'] = ema_alpha*feat_track+ (1-ema_alpha)*feat_det
+            track['feature'] /= np.linalg.norm(feat_track) + 1e-6 # L2 정규화
 
-        row_ind, col_ind = linear_sum_assignment(cost_matrix_2)
-        
-        matches_2 = []
-        unmatched_trackers_2 = list(range(len(unmatched_trks)))
-        unmatched_detections_2 = list(range(len(unmatched_dets)))
-
-        # 매칭된 트래커와 detection을 IoU 임계값에 따라 필터링
-        for r, c in zip(row_ind, col_ind):
-            if 1 - cost_matrix_2[r, c] > iou_threshold:
-                matches_2.append((r, c))
-                unmatched_trackers_2.remove(r)
-                unmatched_detections_2.remove(c)
-
-        # 4. 두번째 매칭에 의해 매칭된 트래커 업데이트 
-        for trk_idx,det_idx in matches_2:
-            t = unmatched_trks[trk_idx]
-            d = unmatched_dets[det_idx]
-            if self.tracked_objects[t]['state'] =='tentative':
-                self.tracked_objects[t]['hits'] +=1
-                self.tracked_objects[t]['time_since_update'] = 0
-                self.tracked_objects[t]['bbox'] = self.tracked_objects[t]['kf'].update(detections[d]['bbox'])
-                self.tracked_objects[t]['z_t_minus_2'] = self.tracked_objects[t]['z_t_minus_1']
-                self.tracked_objects[t]['z_t_minus_1'] = detections[d]['bbox']
-                self.tracked_objects[t]['feature'] = ema_alpha*track['feature'] + (1-ema_alpha)*detection['feature']
-                self.tracked_objects[t]['feature'] /= np.linalg.norm(self.tracked_objects[t]['feature']) + 1e-6
-
-        # 5. 두번째 매칭에 의해 새로 발견한 객체 업데이트(new track)
-        for d in unmatched_detections_2:
+        # 4. 두번째 매칭에 의해 새로 발견한 객체 업데이트(new track)
+        for d in unmatched_dets:
             self.track_id += 1
-            kf = KalmanBox(detections[d]['bbox'])
-            detections[d]['feature']/= np.linalg.norm(detections[d]['feautre']) + 1e-6
+            kf = KalmanBox(detections[d]['bbox']) 
             self.tracked_objects.append({
                 'id': self.track_id,
                 'kf': kf,
@@ -389,29 +350,24 @@ class Deepsort:
                 'age':1,
                 'class': detections[d]['class'],
                 'start_missing_frame_num': 'not-yet',
-                'state' : 'tentative',
-                'hits' : 1,
                 'z_t_minus_1' : detections[d]['bbox'],
                 'z_t_minus_2' : detections[d]['bbox'],
-                'feature' : detections[d]['feature']
+                'feature' : np.zeros(128)
             }) 
-
-        # 6. 트래커 삭제 
+        # 5. 트래커 삭제 
         max_age = 3
-        for t in sorted(unmatched_trackers_2, reverse=True):
+        for t in sorted(unmatched_trks, reverse=True):
             self.tracked_objects[t]['time_since_update'] += 1
-            #tentative 상태에서 삭제
-            if self.tracked_objects[t]['state']=='tentative' and self.tracked_objects[t]['time_since_update'] >= 3:
+            self.tracked_objects[t]['start_missing_frame_num'] = frame_num
+
+            if self.tracked_objects[t]['time_since_update'] >= max_age:
                 del self.tracked_objects[t] # 아예 관측되었던 object에서 삭제
-            #confirm 상태에서 삭제
-            elif self.tracked_objects[t]['state'] == 'confirmed' and self.tracked_objects[t]['time_since_update'] >= max_age:
-                del self.tracked_objects[t]
 
         return self.tracked_objects
 
 # ===================== 메인 =====================
 if __name__ == "__main__":
-    deepsort = Deepsort()
+    deepocsort = DeepOCsort()
     cap = cv2.VideoCapture("dongwon_building-09.avi")
 
     frame_num = 0 
@@ -429,8 +385,8 @@ if __name__ == "__main__":
         
         print('현재',frame_num,'번째 frame')
 
-        detections = deepsort.extract_detections(frame) 
-        tracked = deepsort.update(frame, detections)
+        detections = deepocsort.extract_detections(frame) 
+        tracked = deepocsort.update(frame, detections, frame_num)
 
         # 추적 결과 시각화
         for trk in tracked:
